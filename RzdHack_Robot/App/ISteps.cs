c12cf6d43074debe1fc77601d1a557b009c28778
Ермолаев Александр Алексеевич <ermolaev@rus-telecom.ru>
2024-02-18 18:39:56 +0300

    Удалил неиспользуемые файлы

--- RzdHack_Robot/App/ISteps.cs
+++
@@ -1,9 +0,0 @@
-﻿using RzdHack.RobotbLAbLAbLA.Core;
-
-namespace RzdHack.RobotbLAbLAbLA.App
-{
-    public interface ISteps
-    {
-        public Task Notification(NotificationTask message);
-    }
-}
--- RzdHack_Robot/Core/ResponseToUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RzdHack.RobotbLAbLAbLA.Core
-{
-    public class ResponseToUser
-    {
-        public string Message { get; set; }
-        public long UserId { get; set; }
-    }
-}

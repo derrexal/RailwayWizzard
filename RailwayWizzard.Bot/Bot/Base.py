413a98919ed7ace8fe0a53c3e637be0de3f26dd9
derrexal <derrexal@gmail.com>
2024-07-11 18:29:23 +0300

    #bugfix #bot - fixing bug " No error handlers are registered, logging exception"

--- RailwayWizzard.Bot/Bot/Base.py
+++ RailwayWizzard.Bot/Bot/Base.py
@@ -10,11 +10,14 @@ async def base_error_handler(update: Update, e: Exception, next_step: int, messa
     error_uuid = get_unique_uuid_error()
     message_to_log = f"ERROR [{error_uuid}] {e} "
     message_to_user = f"{message_to_user} <code>{error_uuid}</code>"
-
     logger.error(message_to_log)
-    await update.callback_query.message.reply_text(text=message_to_user,
-                                                   parse_mode=ParseMode.HTML)
-    if next_step == 1:
-        return ConversationHandler.END
-    return next_step - 1
+
+    try:
+        await update.callback_query.message.reply_text(text=message_to_user, parse_mode=ParseMode.HTML)
+    except Exception as e:
+        raise e
+    finally:
+        if next_step == 1:
+            return ConversationHandler.END
+        return next_step - 1
 

090d6b6ea47ab19e4321d1f7e1689f309e71c89d
derrexal <derrexal@gmail.com>
2024-09-02 22:46:33 +0300

    #feature #app Поправил условие при котором сервис не работает с 03 до 4 ночи

--- RailwayWizzard.App/BaseRaiwayWizzardBackgroundService.cs
+++ RailwayWizzard.App/BaseRaiwayWizzardBackgroundService.cs
@@ -1,4 +1,6 @@
-﻿namespace RailwayWizzard.App
+﻿using RailwayWizzard.Shared;
+
+namespace RailwayWizzard.App
 {
     /// <summary>
     /// Воркер который не работает в период с 03:20 по 04:10
@@ -21,11 +23,12 @@
 
         protected override async Task ExecuteAsync(CancellationToken cancellationToken)
         {
-            var todayTime = TimeOnly.FromDateTime(DateTime.Now);
+            var todayTime = TimeOnly.FromDateTime(Common.GetMoscowDateTime);
             
-            while (!cancellationToken.IsCancellationRequested && 
-                todayTime < _startDownTime && 
-                todayTime > _endDownTime)
+            // Сервис РЖД проводит тех. работы
+            var IsDownTime = todayTime > _startDownTime && todayTime < _endDownTime;
+
+            while (!cancellationToken.IsCancellationRequested && !IsDownTime)
             {
                 _logger.LogInformation($"{_nameWorker} running at: {DateTimeOffset.Now}");
 

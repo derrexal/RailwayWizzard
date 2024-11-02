d19d3b56281264414e92ffd742bf9c3bf3150533
derrexal <derrexal@gmail.com>
2024-11-02 22:52:33 +0300

    #feature #app Первая часть избавления от костыля с полем "Updated". Написал миграцию которая проставляет всем существующим задачам это поле в минимальное значение. + Уже проставляю его при создании миграции

---
+++ RailwayWizzard.EntityFramework/Migrations/20241102194118_SetUpdatedFieldToNotificationTaskTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RailwayWizzard.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetUpdatedFieldToNotificationTaskTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"UPDATE AppNotificationTasks SET Updated = {DateTime.MinValue} WHERE Updated IS NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"UPDATE AppNotificationTasks SET Updated = NULL WHERE Id > 0");
+        }
+    }
+}

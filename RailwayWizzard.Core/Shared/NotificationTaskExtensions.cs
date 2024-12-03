e2fb94387e214729f7abe5135492dd6cd609b12b
derrexal <derrexal@gmail.com>
2024-12-03 21:36:20 +0300

    ошибся, вернул обратно

--- RailwayWizzard.Core/Shared/NotificationTaskExtensions.cs
+++ RailwayWizzard.Core/Shared/NotificationTaskExtensions.cs
@@ -12,6 +12,6 @@ namespace RailwayWizzard.Core.Shared
         /// </summary>
         /// <param name="task"></param>
         /// <returns></returns>
-        public static bool IsActuality(this NotificationTask task) => task.DepartureDateTime < Common.MoscowNow;
+        public static bool IsActuality(this NotificationTask task) => task.DepartureDateTime > Common.MoscowNow;
     }
 }

412cf4288c808752981a180b34912455f5f87608
ermolaev <ermolaev@rus-telecom.ru>
2025-03-30 19:01:49 +0300

    #bugfix #app Кажется РЖД изменили АПИ

--- RailwayWizzard.Rzd.ApiClient/Services/GetTrainInformationService/GetTrainInformationService.cs
+++ RailwayWizzard.Rzd.ApiClient/Services/GetTrainInformationService/GetTrainInformationService.cs
@@ -15,36 +15,51 @@ public class GetTrainInformationService: BaseGetDataService, IGetTrainInformatio
     /// <inheritdoc/>
     public async Task<string> GetDataAsync(GetTrainInformationRequest request)
     {
-        var (departureStationCode, arrivalStationCode, departureDateTime, token, getTrainsFromSchedule) = request;
+        var (departureStationCode, arrivalStationCode, departureDateTime, getTrainsFromSchedule) = request;
 
-        var url = $"https://ticket.rzd.ru/apib2b/p/Railway/V1/Search/TrainPricing?service_provider=B2B_RZD&bs={token}";
+        var url = $"https://ticket.rzd.ru/api/v1/railway-service/prices/train-pricing?" +
+                  $"service_provider=B2B_RZD&" +
+                  $"getByLocalTime=true&" +
+                  $"carGrouping=DontGroup&" +
+                  $"origin={departureStationCode}&" +
+                  $"destination={arrivalStationCode}&" +
+                  $"departureDate={departureDateTime:yyyy-MM-ddT00:00:00}&" +
+                  $"specialPlacesDemand=StandardPlacesAndForDisabledPersons&" +
+                  $"carIssuingType=Passenger&" +
+                  $"getTrainsFromSchedule={getTrainsFromSchedule.ToString().ToLower()}&" +
+                  $"adultPassengersQuantity=1&" +
+                  $"childrenPassengersQuantity=0&";
+        
+        // var url = $"https://ticket.rzd.ru/apib2b/p/Railway/V1/Search/TrainPricing?service_provider=B2B_RZD&bs={token}";
 
-        using var requestMessage = new HttpRequestMessage(HttpMethod.Post, url);
+        using var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
         
         requestMessage.Headers.Add("Accept", "application/json, text/plain, */*");
         requestMessage.Headers.Add("Accept-Language", "ru-RU,ru;q=0.9");
         requestMessage.Headers.Add("Connection", "keep-alive");
         requestMessage.Headers.Add("Origin", "https://ticket.rzd.ru");
         requestMessage.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36");
-        requestMessage.Headers.Add("Cookie", " LANG_SITE=ru; " + $"oxxfgh={token}");
-
-        requestMessage.Content = new StringContent(
-            "{" +
-            "\"Origin\":\"" + departureStationCode + "\"," +
-            "\"Destination\":\"" + arrivalStationCode + "\"," +
-            "\"DepartureDate\":\"" + departureDateTime.ToString("yyyy-MM-ddT00:00:00") + "\"," +
-            "\"TimeFrom\":0," +
-            "\"TimeTo\":24," +
-            "\"CarGrouping\":\"DontGroup\"," +
-            "\"GetByLocalTime\":true," +
-            "\"SpecialPlacesDemand\":\"StandardPlacesAndForDisabledPersons\"," +
-            "\"CarIssuingType\":\"All\"," +
-          //"\"GetTrainsFromSchedule\":true}"
-            "\"GetTrainsFromSchedule\":" +
-            getTrainsFromSchedule.ToString().ToLower() +
-            "}"
-            , null
-            , "application/json");
+        // requestMessage.Headers.Add("Cookie", " LANG_SITE=ru; " + $"oxxfgh={token}");
+        requestMessage.Headers.Add("Cookie", " LANG_SITE=ru;");
+        
+        //
+        // requestMessage.Content = new StringContent(
+        //     "{" +
+        //     "\"Origin\":\"" + departureStationCode + "\"," +
+        //     "\"Destination\":\"" + arrivalStationCode + "\"," +
+        //     "\"DepartureDate\":\"" + departureDateTime.ToString("yyyy-MM-ddT00:00:00") + "\"," +
+        //     "\"TimeFrom\":0," +
+        //     "\"TimeTo\":24," +
+        //     "\"CarGrouping\":\"DontGroup\"," +
+        //     "\"GetByLocalTime\":true," +
+        //     "\"SpecialPlacesDemand\":\"StandardPlacesAndForDisabledPersons\"," +
+        //     "\"CarIssuingType\":\"All\"," +
+        //   //"\"GetTrainsFromSchedule\":true}"
+        //     "\"GetTrainsFromSchedule\":" +
+        //     getTrainsFromSchedule.ToString().ToLower() +
+        //     "}"
+        //     , null
+        //     , "application/json");
 
         return await BaseHttpSenderAsync(requestMessage);
     }

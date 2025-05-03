48f03584bd9a2567af81a0620ce287efbdb88188
derrexal <derrexal@gmail.com>
2025-05-03 15:40:21 +0300

    #bot #bug - Fix Event Loop Error

--- RailwayWizzard.Bot/main.py
+++ RailwayWizzard.Bot/main.py
@@ -1,19 +1,15 @@
 import asyncio
-from threading import Thread
-import server
 from bot import telegram_bot
+import server
 
 
 #TODO: Перейти на другой более удобный фреймворк? Почитать про aiogram https://habr.com/ru/articles/757236/
 #TODO: Изучить другой способ параллельного запуска бота и сервера (https://habr.com/ru/articles/709314/)
-def main():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(server.run())
-    loop.close()
-
+async def main():
+    # Запускаем сервер и бота в одной event loop
+    server_task = asyncio.create_task(server.run())
+    bot_task = asyncio.create_task(telegram_bot.run_async())
+    await asyncio.gather(server_task, bot_task)
 
 if __name__ == '__main__':
-    Thread(target=main).start()
-    telegram_bot.run()
-
+    asyncio.run(main())
